e7a8d58b3a8054af35efb795fd6df6f332a55f71
iastanley <iasvks.craigslist@gmail.com>
2017-06-11 23:24:28 -0700

    added fetchPosts action to PostIndex component

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render(){
     return (
       <div>Post Index</div>
@@ -8,4 +14,9 @@ class PostIndex extends Component {
   }
 }
 
-export default PostIndex;
+//no mapStateToProps needed here
+//everything coming from network request
+
+//passing action creator as second arg to connect
+//the same as creating mapDispatchToProps function
+export default connect(null, { fetchPosts })(PostIndex);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions';
 
-export default function PostsReducer(state, action) {
+export default function PostsReducer(state = {}, action) {
   switch(action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
