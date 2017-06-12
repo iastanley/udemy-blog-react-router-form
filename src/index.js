d1efbbb3d14f6722459072dd90021119c4c62adf
iastanley <iasvks.craigslist@gmail.com>
2017-06-12 00:35:40 -0700

    added PostsNew route and Link in PostIndex

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import PostIndex from './components/post_index';
+import PostsNew from './components/posts_new';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -14,7 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex}/>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostIndex}/>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
