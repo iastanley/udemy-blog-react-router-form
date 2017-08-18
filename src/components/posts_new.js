f7712ca76295dc952bfc23686323a1e3dc7dad3f
iastanley <iasvks.craigslist@gmail.com>
2017-08-18 10:49:44 -0700

    refactored PostsNew component to use FIELDS config object

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { createPost } from '../actions';
+
+// refactor to have use FIELDS config object
+const FIELDS = {
+  title: {
+    type: 'input',
+    label: 'Title',
+    errorMsg: 'Enter a title'
+  },
+  categories: {
+    type: 'input',
+    label: 'Categories',
+    errorMsg: 'Enter some categories'
+  },
+  content: {
+    type: 'textarea',
+    label: 'Post Content',
+    errorMsg: 'Enter some content'
+  }
+}
+
 
 class PostsNew extends Component {
   renderField(field) {
-    // use touched if you want error message to show up BEFORE submit
-    // use submitFailed if you want error message to show up only AFTER submit
-    const { label, input, meta: { submitFailed, error } } = field;
-    // ^This syntax is equivalent to:
-    // const { submitFailed, error } = field.meta;
-    // const { label, input } = field;
+    const fieldConfig = FIELDS[field.input.name];
+
+    const { input, meta: { submitFailed, error } } = field;
 
     const className = `form-group ${submitFailed && error ? 'has-danger':''}`;
 
     return (
       <div className={className}>
-        <label>{label}</label>
-        <input
+        <label>{fieldConfig.label}</label>
+        <fieldConfig.type
           className="form-control"
           type="text"
           {...input}
@@ -27,7 +45,7 @@ class PostsNew extends Component {
       </div>
     );
   }
-  // this is where we will handle submitting data to server
+
   onSubmit(values) {
     // use a callback to redirect AFTER network request completes
     this.props.createPost(values, () => {
@@ -35,26 +53,20 @@ class PostsNew extends Component {
     });
   }
 
-  // TODO - figure out later how to make the Post content a textarea not an input
   render() {
-    // prop from redux-form
-    // handleSubmit is a higher order function from redux-form
     const { handleSubmit } = this.props;
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-        <Field
-          label="Title"
-          name="title"
-          component={this.renderField}/>
-        <Field
-          label="Categories"
-          name="categories"
-          component={this.renderField}/>
-        <Field
-          label="Post Content"
-          name="content"
-          component={this.renderField}/>
+      { _.keys(FIELDS).map(key => {
+        return (
+          <Field
+            name={key}
+            key={key}
+            component={this.renderField}
+          />
+        );
+      })}
         <button type="submit" className="btn btn-primary">Save</button>
         <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
@@ -63,30 +75,19 @@ class PostsNew extends Component {
 }
 
 function validate(values) {
-  // values argument is object containing values from form
-  //must return errors object
   const errors = {};
-
-  // validate inputs
-  if (!values.title) {
-    errors.title = 'Enter a title';
-  }
-  if (!values.categories) {
-    errors.categories = 'Enter some categories';
-  }
-  if (!values.content) {
-    errors.content = 'Enter some content';
-  }
-  // return errors object
-  // if errors object is empty redux-form considers input valid
-  // if errors object has any properties input is invalid
+  // lodash each() method takes (value,key) as callback args
+  _.each(FIELDS, (config, field) => {
+    if(!values[field]) {
+      errors[field] = config.errorMsg;
+    }
+  });
   return errors;
 }
 
 export default reduxForm({
   validate,
-  form: 'PostsNewForm' // name for this form - needs to be unique
+  form: 'PostsNewForm'
 })(
-  // passing reduxForm a connected component
   connect(null, { createPost })(PostsNew)
 );
